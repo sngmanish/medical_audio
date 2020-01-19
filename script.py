e224318f5eac5ef658334f051cb3663873784b32
Harsheet-saxena <harshsaxena1999@gmail.com>
2020-01-19 14:03:20 +0530

    text sent to firebase

--- script.py
+++ script.py
@@ -1,38 +1,83 @@
 from threading import Thread
 from queue import Queue
 import speech_recognition as sr
+from time import gmtime, strftime
+import pyrebase
+import json
 
 r = sr.Recognizer()
 audio_queue = Queue()
 
-def recognize_worker():
-    while True:
-        audio = audio_queue.get()
-        try:
-            if audio is None: return
-            
-            text = r.recognize_google(audio)
-            print(text)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
-        except AssertionError:
-            return
-        finally:
-            audio_queue.task_done()
+class Speech:
+    def __init__(self,user):
+        self.user = user
 
-recognize_thread = Thread(target=recognize_worker,daemon=True)
-recognize_thread.start()
+    def getDict(self,s):
+        a = {
+            "text": s,
+            "time": strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        }
+        return a
 
-with sr.Microphone() as source:
-    try:
+    def getJSON(self,text):
+        return json.dumps(self.getDict(text))
+
+    def recognize_worker(self):
         while True:
-            audio_queue.put(r.listen(source))
-    except KeyboardInterrupt:
-        print("this worked")
-        pass
+            audio = audio_queue.get()
+            try:
+                if audio is None: return
 
-audio_queue.join()
-audio_queue.put(None)
-recognize_thread.join()
+                text = r.recognize_google(audio)
+                if(text.lower() == "Logout"):
+                    del self
+                    return
+
+                db.child("Doctor").push(self.getDict(text),self.user["idToken"])
+
+                print(f"sent to firebase: {text}")
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print(f"Could not request results from Google Speech Recognition service; {e}")
+            except AssertionError:
+                return
+            finally:
+                audio_queue.task_done()
+
+if __name__ == '__main__':
+
+    with open("config.json",'r') as configuration_file:
+        config = json.loads(configuration_file.read())
+
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    auth = firebase.auth()
+
+    while(True):
+        email = "test@gmail.com"
+        password = "123456"
+        try:
+            user = auth.sign_in_with_email_and_password(f"{email}",f"{password}")
+            break
+        except Exception as e:
+            print(f"error {e}")
+            continue
+
+    idToken = user["idToken"]
+    speech = Speech(user)
+    recognize_thread = Thread(target=speech.recognize_worker,daemon=True)
+    recognize_thread.start()
+
+    with sr.Microphone() as source:
+        try:
+            while True:
+                audio_queue.put(r.listen(source))
+        except KeyboardInterrupt:
+            print("this worked")
+            pass
+
+    audio_queue.join()
+    audio_queue.put(None)
+    recognize_thread.join()
