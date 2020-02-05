feb0ccb98dc4194814b68898343e874314b68ca6
Manish Singh <manish.bk07@gmail.com>
2020-02-05 12:32:31 +0530

    Dialogueflow Iframe Added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,url_for,redirect
+pytfrom flask import Flask,render_template,request,url_for,redirect
 import pyrebase
 import json
 from time import gmtime, strftime
